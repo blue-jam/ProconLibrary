94cb6140a16d3d418e819248bdde41ab0e5f95f4
blue_jam <blue-jam@users.noreply.github.com>
2016-06-11 21:30:39 +0900

    EACHマクロの書き換え

--- misc/template.cpp
+++ misc/template.cpp
@@ -2,7 +2,7 @@
 using namespace std; 
 
 #define ALL(x) (x).begin(),(x).end() 
-#define EACH(i,c) for(__typeof((c).begin()) i=(c).begin();i!=(c).end();++i)
+#define EACH(i,c) for(auto i=(c).begin();i!=(c).end();++i)
 
 typedef long long ll;
 const double eps = 1e-10;
