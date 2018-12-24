bd9253fcb9b7852ab56b8adae8ce43407c250d19
blue-jam <blue-jam@users.noreply.github.com>
2018-12-24 19:49:47 +0900

    Add document for convex cut

--- geometry/convexcut.cpp
+++ geometry/convexcut.cpp
@@ -1,3 +1,32 @@
+/**
+ * @file
+ *
+ * ## 凸多角形の切断
+ *
+ * 与えられた多角形の直線に対して左側を切除し，右側部分を残す．
+ *
+ * 多角形は反時計回りに点が格納されているものとする．
+ *
+ * ### 計算量
+ *
+ * N角形に対してO(N)
+ *
+ * ### ソースコード
+ *
+ * @include convexcut.cpp
+ *
+ * ### 確認済み問題
+ *
+ *   * AOJ 1283
+ *
+ * ### 使用方法
+ *
+ * 右側を切除したい場合は，if(ccwl[0], l[1], curr) != 1))の1を-1に置き換える．
+ *
+ * ### 参考
+ *
+ *   * [Spaghetti Source](http://www.prefield.com/)
+ */
 // 左側切除
 Polygon convex_cut(const Polygon &p, const Line &l){
     Polygon res;
