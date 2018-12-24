95744032bcf5e0768f35e72178b63cc4abc84669
blue-jam <blue-jam@users.noreply.github.com>
2018-12-24 13:57:25 +0900

    Add document for articulation.cpp

--- graph/articulation.cpp
+++ graph/articulation.cpp
@@ -1,3 +1,21 @@
+/**
+ * @file
+ * ## 関節点，二連結成分分解
+ *
+ * 連結なグラフ \f$G=(V,E)\f$ において頂点 \f$v \in V\f$ を削除した誘導部分グラフ \f$G[V-{v}]\f$ が非連結になるとき，
+ * \f$v\f$ を \f$G\f$ の関節点と呼ぶ．また，任意の頂点を削除しても連結性が保たれる部分グラフを二連結成分と呼ぶ．
+ *
+ * グラフの関節点を見つけるには，深さ優先探索を行えばいい．グラフの任意の点について深さ優先探索を行った時の探索木を考えると，関節点について次の性質がわかる
+ *
+ * - 探索木の根の頂点が関節点である \f$\iff\f$ 探索木の根の子が2つ以上
+ * - 探索木の葉は関節点ではない
+ * - 上記2つを除く頂点vが関節点である \f$\iff\f$ 次の条件を満たすような探索木におけるvの子wが存在する．\f$w\f$ を根とする部分木において，
+ * \f$v\f$ より上に昇る辺を持つ頂点が存在しない．
+ *
+ * ### ソースコード
+ *
+ * @include articulation.cpp
+ */
 void artcRecur(const Graph &g, int v, int u, vector<int> &artc,
                vector<vector<int> > &comp, vector<int> &low,
                vector<bool> &open, stack<int> &S, int &cnt){
@@ -24,8 +42,13 @@ void artcRecur(const Graph &g, int v, int u, vector<int> &artc,
     if(u >= 0 && isArtc || u < 0 && c > 1) artc.push_back(v);
     if(u < 0) S.pop();
 }
-void getArticulation(const Graph &g, vector<int> &artc,
-                     vector<vector<int> > &comp){
+
+/**
+ * グラフ \a g の関節点と二連結成分を \f$O(|V|+|E|)\f$ 時間で計算する．
+ * @param artc 関節点が格納されるベクタ．空であることが求められる．
+ * @param comp 二連結成分が格納されるベクタ．空であることが求められる．
+ */
+void getArticulation(const Graph &g, vector<int> &artc, vector<vector<int> > &comp){
     int n = g.size();
     vector<int> low(n);
     vector<bool> open(n);
