0ecf4306c4d7ef6641c9b7ac7601897b01fadf18
blue-jam <blue-jam@users.noreply.github.com>
2018-12-24 19:07:27 +0900

    Add document for fordfulkerson

--- graph/fordfulkerson.cpp
+++ graph/fordfulkerson.cpp
@@ -1,3 +1,29 @@
+/**
+ * @file
+ * ## 最大流(Ford-Fulkerson)
+ * ### 説明
+ *
+ * 最大フローを求めるアルゴリズム.ネットワークが大きい場合はほかのアルゴリズムを使用すること.
+ *
+ * ### 計算量
+ *
+ *   * O(V E C)
+ *
+ * ただし，Cは最大流量
+ *
+ * ### ソースコード
+ *
+ * @include fordfulkerson.cpp
+ *
+ * ### 確認済み問題
+ *
+ *   * AOJ 2304
+ *
+ * ### 参考
+ *
+ *   * グラフ・ネットワーク・組み合わせ論
+ *   * 蟻本
+ */
 Weight ford_dfs(Graph &g, int v, int t, Weight f, vector<int> &used){
     if(v == t) return f;
     used[v] = true;
@@ -13,6 +39,10 @@ Weight ford_dfs(Graph &g, int v, int t, Weight f, vector<int> &used){
     }
     return 0;
 }
+/**
+ * グラフの @a s から @a t への最短経路を計算する．
+ * 事前に逆辺をweight=0で追加しておく．また，グラフは書き換えを行うので必要な場合はコピーを取っておくこと．
+ */
 Weight ford_fulkerson(Graph &g, int s, int t){
     vector<int> used(g.size());
     Weight flow = 0;
