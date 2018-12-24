a075e24e563873e6fbe3e67d47c1973e9dd9ef78
blue-jam <blue-jam@users.noreply.github.com>
2018-12-24 13:44:02 +0900

    Add document for topological sort

--- graph/topologicalsort.cpp
+++ graph/topologicalsort.cpp
@@ -1,5 +1,23 @@
-bool topologicalRecur(const Graph &g, int v, vector<int> &order,
-                      vector<int> &color){
+/**
+ * @file
+ *
+ * ### トポロジカルソート
+ *
+ * 有向グラフgについて，頂点集合Vに対して順序を付け，v[i](0<=i<|V|)と並べたとする．
+ * v[i]からv[j]に向かう枝が存在するならば，i<jが成り立つ時，この順序付けをトポロジカル順序と呼ぶ．
+ *
+ * トポロジカル順序を求めることをトポロジカルソートと呼び，DFSの帰りがけの順に頂点に番号を振っていくことでその逆順が求められる．
+ *
+ * ### ソースコード
+ *
+ * @include topologicalsort.cpp
+ *
+ * ### 確認済み問題
+ *
+ * - AOJ 2222(Alien's Counting)
+ */
+
+bool topologicalRecur(const Graph &g, int v, vector<int> &order, vector<int> &color){
     color[v] = 1;
     EACH(i, g[v]){
         if(color[i->to] == 1) return false;
@@ -9,6 +27,12 @@ bool topologicalRecur(const Graph &g, int v, vector<int> &order,
     order.push_back(v); color[v] = 2;
     return true;
 }
+
+/**
+ * グラフ \a g のトポロジカル順序を\f$O(\|V\|+\|E\|)\f$ 時間で計算する．
+ * @param order トポロジカル順序が格納されるベクタ．空のベクタを渡す．
+ * @return DAGであれば @a true を返す．
+ */
 bool topologicalSort(const Graph &g, vector<int> &order){
     int n = g.size();
     vector<int> color(n);
