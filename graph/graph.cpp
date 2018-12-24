71846ae54b667f39b097d50d80af4696f10a33ec
blue-jam <blue-jam@users.noreply.github.com>
2018-12-24 13:35:21 +0900

    Add document for graph

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,3 +1,26 @@
+/**
+ *
+ * ### グラフの要素
+ * グラフは各頂点から出て行く辺をリストとして保持する隣接リスト表現と各頂点間に辺が存在する情報を記録する隣接行列表現がある．
+ * それぞれ利点があるので状況に応じて使い分けること．
+ *
+ * <dl>
+ *   <dt>隣接リスト</dt><dd>頂点を基準に考えればいいときに有向．重複辺も扱える．</dd>
+ *   <dt>隣接行列</dt><dd>@f$O(E)@f$ が @f$O(V^2)@f$ になる．密グラフや，頂点数が少ないグラフについて効率よく扱える．重複辺を扱いにくい．</dd>
+ * </dl>
+ *
+ * ### ソースコード
+ *
+ * @include graph.cpp
+ *
+ * Edgeのless演算子のオーバーロードで，大きいほうが左辺のときにtrueを返すようにしている．
+ * これはpriority_queueで，weightの小さいほうを先に取り出すときの工夫である．
+ * 気に入らない場合は，greater演算子をオーバーロードし，priority_queueを以下のように宣言する．
+ *
+ * @code
+ * priority_queue<Edge>, vector<Edge>, greater<Edge>> Q;
+ * @endcode
+ */
 typedef int Weight;
 struct Edge{
     int from, to;
