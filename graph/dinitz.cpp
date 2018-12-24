d792061f77dffa3908f080c90069ee6da2eaaafa
blue-jam <blue-jam@users.noreply.github.com>
2018-12-24 19:16:36 +0900

    Add document for dinitz

--- graph/dinitz.cpp
+++ graph/dinitz.cpp
@@ -1,3 +1,27 @@
+/**
+ * @file
+ * ## 最大流(Dinitz)
+ *
+ * 多くの文献で"Dinic"として紹介されているが，原論文を英訳するときに著者名がそう訳されたためそちらが広まっただけで，その後の自著では"Dinitz"と表記されている．
+ *
+ * ネットワークをBFSで層別ネットワークに分け，その中での極大フローを探し，増加させることを繰り返し最大流量を求める．
+ *
+ * ### 計算量
+ *
+ *   * O(V^2 E)
+ *
+ * ### ソースコード
+ *
+ * @include dinitz.cpp
+ *
+ * ### 確認済み問題
+ *
+ *   * AOJ 2304
+ *
+ * ### 参考
+ *
+ *   * グラフ・ネットワーク・組合せ論
+ */
 Weight blockingFlow(Graph &g, vector<int> &l, int v, int t, Weight f){
     if(v == t) return f;
     EACH(i, g[v]){
@@ -12,6 +36,9 @@ Weight blockingFlow(Graph &g, vector<int> &l, int v, int t, Weight f){
     }
     return 0;
 }
+/**
+ *Ford-Fulkersonと同様にネットワークとソース，シンクの頂点を渡す．グラフを構築するときに逆辺を付け忘れないように．
+ */
 Weight dinitz(Graph &g, int s, int t){
     int n = g.size();
     Weight flow = 0;
